5850136731ed7ee8466155129f379c2e82fdc293
Nicolas <76515190+NicolasMB3@users.noreply.github.com>
2022-02-16 12:11:28 +0100

    Code is now working :100:

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,33 +1,52 @@
-const inputValue = document.querySelector('#command');
 const textTerminal = document.getElementById('terminal-text');
+let inputValue = document.querySelector('#command');
 
-function writeAnwser(text) {
-   const newDiv = document.createElement('div');
-   const newContent = document.createTextNode(`C:\\Users\\Nicolas> ` + text);
-   newDiv.append(newContent);
-   textTerminal.appendChild(newDiv);
-   inputValue.disabled = true;
+function inputToText() {
+   // Stockage de la valeur écrite
+   const tempDataInput = inputValue.value;
+   removeInput();
+   textTerminal.innerHTML +=  `
+   <div id="terminal-input-new">
+      <span class="text--bold" id="terminal-title">C:\\Users\\Nicolas></span>
+      <span id="teest">` + tempDataInput + `</span>
+   </div>`;
+}
+
+function removeInput() {
+   let inputValue2 = document.querySelector('#terminal-input');
+   inputValue2.remove();
 }
 
 function newInput() {
-   const tempDataInput = inputValue.value;
-   const inputTitle = document.getElementById('terminal-title');
    textTerminal.innerHTML += 
       `<div id="terminal-input">
          <p><span class="text--bold">C:\\Users\\Nicolas></span></p>
-         <input type="text" id="command" name="command" required minlength="4" maxlength="8" autocomplete="off" autofocus class="test">
+         <input type="text" id="command" name="command" required minlength="4" maxlength="8" autocomplete="off" autofocus>
       </div>`
-   inputValue.focus()
+   inputValue = document.querySelector('#command');
+   inputValue.focus();
+   pressKey();
+}
+
+function writeAnwser(text) {
+   const newDiv = document.createElement('div');
+   const newContent = document.createTextNode(`Information : ` + text);
+   newDiv.append(newContent);
+   textTerminal.appendChild(newDiv);
+   inputValue.disabled = true;
 }
 
 function pressKey() {
    inputValue.addEventListener("keypress", (e) => {
       if (e.key === 'Enter') {
          if(inputValue.value === 'help') {
+            inputToText();
             writeAnwser(`La commande help n'est pas encore disponible`);
             newInput();
          } else {
-            writeAnwser(`La commande ` + inputValue.value + ` n'est pas disponible`)
+            inputToText();
+            writeAnwser(`La commande ` + inputValue.value + ` n'est pas disponible pour le moment ..`);
+            newInput();
          }
       }
    });
