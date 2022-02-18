535567b8d99e9f64b148b9be9543abf4f980bad2
Nicolas <76515190+NicolasMB3@users.noreply.github.com>
2022-02-18 04:43:45 +0100

    Optimization :tada:

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -137,7 +137,7 @@ function snapping() {
          modifiers: [
             interact.modifiers.snap({
             targets: [
-               interact.snappers.grid({ x: 30, y: 30 })
+               interact.snappers.grid({ x: 25, y: 25 })
             ],
             range: Infinity,
             relativePoints: [ { x: 0, y: 0 } ]
@@ -162,6 +162,59 @@ function snapping() {
    document.getElementById('image-desktop-folder').onclick = onClick;
 }
 
+function resize() {
+   interact('.resize-drag')
+  .resizable({
+    // resize from all edges and corners
+    edges: { left: false, right: true, bottom: true, top: false },
+
+    listeners: {
+      move (event) {
+        var target = event.target
+        var x = (parseFloat(target.getAttribute('data-x')) || 0)
+        var y = (parseFloat(target.getAttribute('data-y')) || 0)
+
+        // update the element's style
+        target.style.width = event.rect.width + 'px'
+        target.style.height = event.rect.height + 'px'
+
+        // translate when resizing from top or left edges
+        x += event.deltaRect.left
+        y += event.deltaRect.top
+
+        target.style.transform = 'translate(' + x + 'px,' + y + 'px)'
+
+        target.setAttribute('data-x', x)
+        target.setAttribute('data-y', y)
+      }
+    },
+    modifiers: [
+      // keep the edges inside the parent
+      interact.modifiers.restrictEdges({
+        outer: 'parent'
+      }),
+
+      // minimum size
+      interact.modifiers.restrictSize({
+        min: { width: 726, height: 620 }
+      })
+    ],
+
+    inertia: true
+  })
+  .draggable({
+    listeners: { move: window.dragMoveListener },
+    inertia: true,
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent',
+        endOnly: true
+      })
+    ]
+  })
+}
+
+resize();
 snapping();
 dragAndDrop();
 time();
